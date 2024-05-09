7a545bfc5e3ccf2d0550242ad08e426b37e39ebb
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-05-09 14:39:20 +0200

    helper method is initially added

--- src/helper.py
+++ src/helper.py
@@ -1,16 +1,34 @@
 import os
 import sys
+import torch
+import torch.optim as optim
 
 sys.path.append("src/")
 
-from utils import load
+from utils import load, config
+from generator import Generator
+from discriminator import Discriminator
 from loss.gan_loss import GANLoss
 from loss.cycle_loss import CycleLoss
 from loss.pixel_loss import PixelLoss
 
 
 def load_dataloader():
-    pass
+    if os.path.exists(config()["path"]["processed_path"]):
+        path = config()["path"]["processed_path"]
+
+        train_dataloader = load(filename=os.path.join(path, "train_dataloader.pkl"))
+        test_dataloader = load(filename=os.path.join(path, "test_dataloader.pkl"))
+        dataloader = load(filename=os.path.join(path, "dataloader.pkl"))
+
+        return {
+            "train_dataloader": train_dataloader,
+            "test_dataloader": test_dataloader,
+            "dataloader": dataloader,
+        }
+
+    else:
+        raise Exception("Can't load dataloader".capitalize())
 
 
 def helpers(**kwargs):
@@ -19,15 +37,52 @@ def helpers(**kwargs):
     adam = kwargs["adam"]
     SGD = kwargs["SGD"]
 
-    # For Adam
+    out_channels = 64
 
-    # For SGD
+    netG_XtoY = Generator(in_channels=in_channels, out_channels=in_channels)
+    netG_YtoX = Generator(out_channels=in_channels, out_channels=in_channels)
 
-    # Loss
+    netD_X = Discriminator(in_channels=in_channels, out_channels=out_channels)
+    netD_Y = Discriminator(in_channels=in_channels, out_channels=out_channels)
 
-    # dataloader
+    if adam:
+        optimizerG = optim.Adam(
+            params=list(netG_XtoY.parameters()) + list(netG_YtoX.parameters()),
+            lr=lr,
+            betas=(0.5, 0.999),
+        )
+        optimizerD_X = optim.Adam(params=netD_X.parameters(), lr=lr, betas=(0.5, 0.999))
+        optimizerD_Y = optim.Adam(params=netD_Y.parameters(), lr=lr, betas=(0.5, 0.999))
 
-    # Return
+    elif SGD:
+        optimizerG = optim.SGD(
+            params=list(netG_XtoY.parameters()) + list(netG_YtoX.parameters()),
+            lr=lr,
+        )
+        optimizerD_X = optim.SGD(params=netD_X.parameters(), lr=lr, momentum=0.95)
+        optimizerD_Y = optim.SGD(params=netD_Y.parameters(), lr=lr, momentum=0.95)
+
+    adversarial_loss = GANLoss()
+    cycle_loss = CycleLoss()
+    pixel_loss = PixelLoss()
+
+    dataloader = load_dataloader()
+
+    return {
+        "netG_XtoY": netG_XtoY,
+        "netG_YtoX": netG_YtoX,
+        "netD_X": netD_X,
+        "netD_Y": netD_Y,
+        "optimizerG": optimizerG,
+        "optimizerD_X": optimizerD_X,
+        "optimizerD_Y": optimizerD_Y,
+        "adversarial_loss": adversarial_loss,
+        "cycle_loss": cycle_loss,
+        "pixel_loss": pixel_loss,
+        "dataloader": dataloader["dataloader"],
+        "train_dataloader": dataloader["train_dataloader"],
+        "test_dataloader": dataloader["test_dataloader"],
+    }
 
 
 if __name__ == "__main__":
