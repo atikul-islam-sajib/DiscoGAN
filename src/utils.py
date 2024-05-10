f5a67efd0dd0234ead4ea5534c16e49c19b3649b
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-05-10 13:23:47 +0200

    config file is updated

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,8 @@
 import os
 import yaml
 import joblib
+import torch
+import torch.nn as nn
 
 
 def config():
@@ -18,3 +20,25 @@ def dump(value=None, filename=None):
 def load(filename=None):
     if filename is not None:
         return joblib.load(filename=filename)
+
+
+def device_init(device="mps"):
+    if device == "mps":
+        return torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+
+    elif device == "cuda":
+        return torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    else:
+        return torch.device("cpu")
+
+
+def weights_init(m):
+    classname = m.__class__.__name__
+
+    if classname.find("Conv") != -1:
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+
+    elif classname.find("BatchNorm") != -1:
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0.0)
